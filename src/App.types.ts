674855a9271d04b79a9a070b076e74ab180c3cac
Atul Nirbhavane <anirbhavane106@gmail.com>
2024-05-05 17:22:24 +0530

    initial commit

---
+++ src/App.types.ts
@@ -0,0 +1,18 @@
+export interface AppProps {
+    title: string;
+  }
+  
+  export interface Name {
+    first: string;
+    last: string;
+  }
+  
+  export interface Login {
+    uuid: string;
+  }
+  
+  export interface Users {
+    name: Name;
+    login: Login;
+    email: string;
+  }
